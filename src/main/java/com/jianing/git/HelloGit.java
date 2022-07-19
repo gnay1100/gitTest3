5ea0c27348ff84be0ccef85c9abf80cf505de3f7
jianing <111@qq.com>
2022-07-19 19:44:03 +0800

    third commit

--- src/main/java/com/jianing/git/HelloGit.java
+++ src/main/java/com/jianing/git/HelloGit.java
@@ -9,6 +9,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
-        System.out.println("hello3");
+        System.out.println("hello-3");
     }
 }
