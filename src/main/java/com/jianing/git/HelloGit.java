ce4dcb92801e057b20b718e015dfdee02d45e4d7
jianing <111@qq.com>
2022-07-19 19:20:14 +0800

    first commit

---
+++ src/main/java/com/jianing/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.jianing.git;
+
+/**
+ * @author jianning
+ * @create 2022-07-19 2022/7/19
+ * @description 第一次提交
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
